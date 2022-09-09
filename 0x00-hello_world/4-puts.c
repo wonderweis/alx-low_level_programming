ce5d6b4edc95533ed72f17bc41e40687db9788ef
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-08 20:19:21 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main: is the entry point for this program
+ * main- is the entry point for this program
  *
- * return: ends the program and shows the value of zero (0)
+ * Return: ends the program and shows the value of zero (0)
  */
 int main(void)
 {
