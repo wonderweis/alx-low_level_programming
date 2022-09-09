fc58c536a1e7e6cf8c0c3949c316bb2d8de1ac3a
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-08 20:06:20 -0700

    Hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main is the entry point for this program
+ * return ends the program and shows the value of zero
+ * */
+int main(void)
+{
+	puts("\"Programming is like buildinga multilingual puzzle");
+	return (0);
+}
