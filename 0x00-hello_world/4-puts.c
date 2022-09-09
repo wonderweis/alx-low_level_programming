b2002f642c28054ce7b21115ff0c9593eb62b10e
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-08 20:10:20 -0700

    Hellp, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main is the entry point for this program
- * return ends the program and shows the value of zero
- * */
+ * return ends the program and shows the value of zero (0)
+  */
 int main(void)
 {
-	puts("\"Programming is like buildinga multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
