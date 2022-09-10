98f0e8242832078591edc545469e3b4891db21f8
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-09 19:39:50 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point to declare variable and code
+ *
+ * Return: the value of 0 always
+ */
+
+int main(void)
+{
+	char ch;
+
+	putchar("\nLowercase characters : \n");
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar("%c ", ch);
+	}
+	return (0);
+}
