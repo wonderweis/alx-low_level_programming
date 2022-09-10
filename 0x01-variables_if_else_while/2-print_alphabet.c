355d3de657302811828b55ea4ac90fc87a176629
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-10 01:26:52 -0700

    Lowercase'

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	char letter;
+	char ch;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
