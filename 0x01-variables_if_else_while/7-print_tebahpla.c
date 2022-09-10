afd4596dda4a21d44ed9fd99ef947e8071fa28dc
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-10 02:23:31 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- prints the lowercase alphabet in reverse
+ *
+ * Return: always the value 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
