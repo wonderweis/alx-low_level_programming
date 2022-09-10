6ecec29c9cdead93ba5330a9b7df9fb64336d40f
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-10 01:48:41 -0700

    remove q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- program to print all letters except q and e
+ *
+ * Return: the value of 0 always
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n')
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 This program will assign a random number to the variable n each time it is executed
+program that prints the alphabet in lowercase, followed by a new line.
