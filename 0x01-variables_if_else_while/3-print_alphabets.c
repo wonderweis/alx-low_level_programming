9a8d283b07ed051341ad6b8922023a867e2ef4a2
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-10 01:31:06 -0700

    lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char letter;
+	char ch;
 	
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (ch = 'a'; ch <= 'z';ch++)
+		putchar(ch);
 	
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 	
 	putchar('\n');
 	
