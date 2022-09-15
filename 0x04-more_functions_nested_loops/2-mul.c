637712ae50addc32cce079fec6a528ca32d9d3fc
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-15 16:05:29 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul- function that multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: expected value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 More nested loops
 fucntion that checks for a digit
+function that multiplies two intgers
