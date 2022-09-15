d486dcab9ffb3bbbc92adf0e705c30fb2619eff3
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-14 19:09:56 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print a - z ten times
+ *
+ * Return: value 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
