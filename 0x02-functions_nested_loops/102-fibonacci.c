9d42fbd7e13a3a3b87cb903f65992c39405efac6
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-14 20:41:28 -0700

    fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- prints the first 50 Fibonacci numbers
+ *
+ * Return: value of 0
+ */
+
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	return (0);
+}
