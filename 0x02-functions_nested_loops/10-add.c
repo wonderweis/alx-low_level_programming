e25e50ae39e5f71c160f2262da8b395806a77bef
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-14 20:12:19 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add- function that adds two integers and returns the result
+ * @i: print int i
+ * @k: print int k
+ *
+ * Return: value of 0
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
