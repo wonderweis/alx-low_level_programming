ea139b5c4b0c5fa45b4065ef893a65d376a8a8d0
CHIDUBEM EMMANUEL <emmanuelgerald05@gmail.com>
2022-09-15 16:50:45 -0700

    sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square- prints a square
+ * @size: value
+ */
+
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
