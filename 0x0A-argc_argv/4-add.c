6a6b2e56ce95a64b68ce1c43398d32caf8f6c6fc
wonderweis <emmanuelgerald05@gmail.com>
2022-10-02 16:16:17 -0700

    To infinity and beyond - buzlightyear

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: first arg
+ * @argv: second arg
+ *
+ * Return: value
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
