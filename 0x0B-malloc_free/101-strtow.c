365f6995d586761ed410525b211cb6a02fd8190a
wonderweis <emmanuelgerald05@gmail.com>
2022-10-03 13:56:36 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,7 +4,6 @@
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
- *
  * Return: number of words
  */
 
@@ -21,7 +20,6 @@ int count_word(char *s)
 		{
 			flag = 0;
 		}
-
 		else if (flag == 0)
 		{
 			flag = 1;
@@ -44,22 +42,16 @@ char **strtow(char *str)
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
-	{
 		len++;
-	}
 	words = count_word(str);
-
 	if (words == 0)
-	{
 		return (NULL);
-	}
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -69,28 +61,20 @@ char **strtow(char *str)
 				end = i;
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
-				{
 					return (NULL);
-				}
-
 				while (start < end)
-				{
 					*tmp++ = str[start++];
-				}
-
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
 				c = 0;
 			}
 		}
-
 		else if (c++ == 0)
 		{
 			start = i;
 		}
 	}
-
 	matrix[k] = NULL;
 	return (matrix);
 }
